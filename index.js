f7d179de9eb52b9a469b8305f452505daa66a74c
Arielle Ross-Levy <ariellerosslevy@Arielles-MacBook-Air.local>
2021-03-19 13:06:38 -0600

    created db, migrated, seeded

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 4000
+const connection = require('./knexfile').development
+const database = require('knex')(connection)
+const cors = require('cors')
+
+app.use(cors())
+//makes sure body is parsed from json when its given as a request
+app.use(express.json())
+
+app.get('/drawings', (_, response) => {
+    database('drawings')
+        .then(drawings => response.send(drawings))
+})
 
 app.get('/', (request, response)=> {
     response.json({message: "made it"})
