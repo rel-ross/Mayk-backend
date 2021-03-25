1de44e44ea03eb735deec2f9047d67a798f861e2
Arielle Ross-Levy <ariellerosslevy@Arielles-MacBook-Air.local>
2021-03-25 09:28:12 -0600

    trying to get uplad to node and aws working

--- index.js
+++ index.js
@@ -5,14 +5,49 @@ const bodyParser = require('body-parser')
 const connection = require('./knexfile').development
 const database = require('knex')(connection)
 const cors = require('cors')
+const router = express.Router();
+const aws = require("aws-sdk");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+
+const s3 = new aws.S3({
+    apiVersion: "2012-10-17",
+    region: process.env["us-east-2"],
+    credentials: {
+        secretAccessKey: process.env.aec187SZgAMAKxvUHlz9hoHRcwvxnzx23Xvq3pw,
+        accessKeyId: process.env.AKIAVTHVA37UVSDLKY4R
+    }
+});
+
+const upload = multer({
+    storage: multerS3({
+        s3, // The s3 instance from above
+        // The name of your S3 bucket
+        bucket: process.env["mayk-capstone"],
+        key: (request, file, next) => {
+            // This names the file. This example prepends the
+            // UNIX timestamp to original name of the file,
+            // which helps with duplicate file names
+            next(null, `files/${Date.now()}_${file.originalname}`);
+        }
+    })
+});
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-
-
 app.use(cors())
-//makes sure body is parsed from json when its given as a request
 app.use(express.json())
+
+router.post("/upload", upload.single("file"), (request, response) => {
+    // Return the URL the file was uploaded to- optionally, store it
+    // in a database first.
+    response.json({data: request.file.location});
+});
+
+module.exports = router;
+
+
+
 
 app.get('/drawings', (_, response) => {
     database('drawings')
